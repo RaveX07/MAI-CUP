b3d5b2db5b40a94a95a981ac7c20137896b0fed0
RaveX07 <jannis.schroeter@gmx.de>
2022-05-23 15:22:15 +0200

    improving turn mehtod

--- test/turn.cpp
+++ test/turn.cpp
@@ -1,77 +1,55 @@
-#include <Arduino.h>
-#include <HCSR04.h>
+#include <SR04.h>
+#include "Arduino.h"
+int LED = 15;
 
 //right motor
 #define PWM_PIN_FORWARD_RIGHT 5
 #define PWM_PIN_BACKWARD_RIGHT 4
-#define ENABLE_PIN_RIGHT 6
+#define ENABLE_PIN_RIGHT_1 6
 
 //left motor
-#define PWM_PIN_FORWARD_LEFT 9
-#define PWM_PIN_BACKWARD_LEFT 8
-#define ENABLE_PIN_LEFT 10
+#define PWM_PIN_FORWARD_LEFT 8
+#define PWM_PIN_BACKWARD_LEFT 9
+#define ENABLE_PIN_LEFT_1 10
 
-//ir sensors 
-#define IR_SENSOR_LEFT A0
-#define IR_SENSOR_RIGHT A1
+//left US sensor
+const int triggerLeft = 40;
+const int echoLeft = 38;
+long distanceLeft;
+SR04 sr04 = SR04(echoLeft, triggerLeft);
 
-//US-Sensors
-#define ECHO_MIDDLE 48
-#define TRIGGER_MIDDLE 50
+//front US sensor works
+const int triggerFront = 50;
+const int echoFront = 48;
+long distanceFront;
+SR04 sr05 = SR04(echoFront, triggerFront);
 
-#define ECHO_RIGHT 49
-#define TRIGGER_RIGHT 51
+//right US sensor works
+const int triggerRight = 51;
+const int echoRight = 49;
+long distanceRight;
+SR04 sr06 = SR04(echoRight, triggerRight);
 
-#define ECHO_LEFT 38
-#define TRIGGER_LEFT 40
+// right IR sensor
+const int IRSensorRight = A1;
+int valueRight = 0;
 
-//hall sensors 
-#define HALL_SENSOR_LEFT 12
-#define HALL_SENSOR_RIGHT 13
+//left IR sensor
+const int IRSensorLeft = A0;
+int valueLeft = 0;
 
-int distanceLeft;
-int distanceRight;
-int distanceMiddle;
+//variables for IR-Sensors
+bool lineLeft;
+bool lineRight;
 
-UltraSonicDistanceSensor distanceSensorMiddle(ECHO_MIDDLE, TRIGGER_MIDDLE);
-UltraSonicDistanceSensor distanceSensorRight(ECHO_RIGHT, TRIGGER_RIGHT);
-UltraSonicDistanceSensor distanceSensorLeft(ECHO_LEFT, TRIGGER_LEFT);
-
-void readSensors(){
-    Serial.println(distanceSensorMiddle.measureDistanceCm());
-    Serial.println(distanceSensorRight.measureDistanceCm());
-    Serial.println(distanceSensorLeft.measureDistanceCm());
-
-    distanceMiddle = distanceSensorMiddle.measureDistanceCm();
-    distanceRight = distanceSensorRight.measureDistanceCm();
-    distanceLeft = distanceSensorLeft.measureDistanceCm();
-}
-
-
-void drive(int speed)
-{
-  analogWrite(PWM_PIN_FORWARD_RIGHT, speed);
+void drive(int speed){
+  digitalWrite(ENABLE_PIN_LEFT_1, HIGH);
+  digitalWrite(ENABLE_PIN_RIGHT_1, HIGH);
+  analogWrite(PWM_PIN_FORWARD_RIGHT, speed*1.1);
   analogWrite(PWM_PIN_BACKWARD_RIGHT, 0);
   analogWrite(PWM_PIN_FORWARD_LEFT, speed);
   analogWrite(PWM_PIN_BACKWARD_LEFT, 0);
-}
 
-void turnCustom(char direction, int speedMax, int speedMin)
-{
-  if (direction == 'r')
-  {
-    analogWrite(PWM_PIN_FORWARD_RIGHT, speedMin);
-    analogWrite(PWM_PIN_BACKWARD_RIGHT, 0);
-    analogWrite(PWM_PIN_FORWARD_LEFT, speedMax);
-    analogWrite(PWM_PIN_BACKWARD_LEFT, 0);
-  }
-  else if (direction == 'l')
-  {
-    analogWrite(PWM_PIN_FORWARD_RIGHT, speedMax);
-    analogWrite(PWM_PIN_BACKWARD_RIGHT, 0);
-    analogWrite(PWM_PIN_FORWARD_LEFT, speedMin);
-    analogWrite(PWM_PIN_BACKWARD_LEFT, 0);
-  }
 }
 
 void rotate(char direction, int speed)
@@ -92,64 +70,123 @@ void rotate(char direction, int speed)
   }
 }
 
-void setup () {
-    Serial.begin(115200);
+void turnCustom(char direction, int speedMax, int speedMin)
+{
+  if (direction == 'l')
+  {
+    analogWrite(PWM_PIN_FORWARD_RIGHT, speedMin);
+    analogWrite(PWM_PIN_BACKWARD_RIGHT, 0);
+    analogWrite(PWM_PIN_FORWARD_LEFT, speedMax);
+    analogWrite(PWM_PIN_BACKWARD_LEFT, 0);
+  }
+  else if (direction == 'r')
+  {
+    analogWrite(PWM_PIN_FORWARD_RIGHT, speedMax);
+    analogWrite(PWM_PIN_BACKWARD_RIGHT, 0);
+    analogWrite(PWM_PIN_FORWARD_LEFT, speedMin);
+    analogWrite(PWM_PIN_BACKWARD_LEFT, 0);
+  }
+}
 
-    // wait until serial port opens for native USB devices
 
+void readIRSensors(){
+  valueLeft = analogRead(IRSensorLeft);
+  valueRight = analogRead(IRSensorRight);
+  Serial.print(valueLeft);
+  if (valueLeft >= 700){
+    lineLeft = true;
+    Serial.println("; line detected left");
+  } else {
+    lineLeft = false;
+    Serial.println("; no line left");
+  }
+  Serial.print(valueRight);
+  if (valueRight >= 700){
+    lineRight = true;
+    Serial.println("; line detected right");
+  } else {
+    lineRight = false;
+    Serial.println("; no line right");
+  }
+
+}
+
+
+
+
+void turn(){
+
+    char direction;
+    
+    while (distanceRight < 40 && distanceLeft < 40)
+    { // while no curve is detected: drive forward
+        drive(100);
+        readUSSensors();
+        readIRSensors();
+    }
+
+    if(distanceLeft > 40){
+        direction = 'l';
+    } else {
+        direction = 'r';
+    }
+
+    switch (direction)
+    {
+    case 'r':
+
+        rotate('r', 100);
+
+        delay(500); // rotate for 0.5 seconds
+
+        drive(100); // drive forward for 0.5 seconds
+
+        delay(500);
+        break;
+    
+    case 'l':
+
+        rotate('l', 100);
+
+        delay(500); // rotate for 0.5 seconds
+
+        drive(100); // drive forward for 0.5 seconds
+
+        delay(500);
+
+    default:
+        break;
+    }
+
+}
+
+void readUSSensors(){
+    distanceLeft = sr04.Distance();
+    distanceFront = sr05.Distance();
+    distanceRight = sr06.Distance();
+    Serial.println("left:" + String(distanceLeft) + "cm; front:" + String(distanceFront) + "cm; right:" + String(distanceRight) +  "cm");  
+}
+
+void setup(){
+    Serial.begin(9600);
     pinMode(PWM_PIN_FORWARD_LEFT, OUTPUT);
     pinMode(PWM_PIN_BACKWARD_LEFT, OUTPUT);
-    pinMode(ENABLE_PIN_LEFT, OUTPUT);
+    pinMode(ENABLE_PIN_LEFT_1, OUTPUT);
 
     pinMode(PWM_PIN_FORWARD_RIGHT, OUTPUT);
     pinMode(PWM_PIN_BACKWARD_RIGHT, OUTPUT);
-    pinMode(ENABLE_PIN_RIGHT, OUTPUT);
+    pinMode(ENABLE_PIN_RIGHT_1, OUTPUT);
 
-    pinMode(IR_SENSOR_LEFT, INPUT);
-    pinMode(IR_SENSOR_RIGHT, INPUT);
+    digitalWrite(ENABLE_PIN_LEFT_1, HIGH);
+    digitalWrite(ENABLE_PIN_RIGHT_1, HIGH);
 
-    pinMode(HALL_SENSOR_LEFT, INPUT);
-    pinMode(HALL_SENSOR_RIGHT, INPUT);
-
-    pinMode(TRIGGER_MIDDLE, OUTPUT);
-    pinMode(TRIGGER_RIGHT, OUTPUT);
-    pinMode(ECHO_MIDDLE, INPUT);
-    pinMode(ECHO_MIDDLE, INPUT);
-
-    digitalWrite(ENABLE_PIN_LEFT, HIGH);
-    digitalWrite(ENABLE_PIN_RIGHT, HIGH);
+    pinMode(LED, OUTPUT);
 
     Serial.println("Setup complete ");
 }
 
-void loop () {
+void loop(){
 
-    readSensors();
-
-    while(distanceLeft < 40 && distanceRight < 40){
-      drive(125);
-    }
-
-    delay(200);                       //keep driving forward for 0.2 seconds
-
-    if(distanceLeft >= 40){
-      rotate('l', 100);                  
-
-      delay(500);                       //rotate for 0.5 seconds
-
-      drive(125);                        //drive forward for 0.5 seconds
-
-      delay(500);
-
-    } else if(distanceRight >= 40){
-      rotate('r', 100);                  
-
-      delay(500);                       //rotate for 0.5 seconds
-
-      drive(125);                        //drive forward for 0.5 seconds
-
-      delay(500);
-
-    }
+    turn();
 
 }
